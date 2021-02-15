8f41e9e0299effd9b76dd0f1fdf55d5a63f65efa
Tien Manh <manh.dang2904@hcmut.edu.vn>
2021-02-15 16:27:42 +0700

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -4,8 +4,11 @@ const hbs=require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-//Define path for the Express config
 const app=express()
+const port=process.env.PORT || 3000
+
+//Define path for the Express config
+
 const publicDirectory=path.join(__dirname,'../public')
 const viewsPath=path.join(__dirname,'../templates/views')
 const partialsPath=path.join(__dirname,'../templates/partials')
@@ -81,6 +84,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000')
+app.listen(port,()=>{
+    console.log('Server is up on port'+port)
 })
